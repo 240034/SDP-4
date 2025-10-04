cfaecd33d75b0ecc76b8e2958bc688e4a67053d1
240034 <240034@astanait.edu.kz>
2025-10-04 20:13:27 +0500

    Create Radio.java

---
+++ Radio.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public class Radio implements Device {
+    @Override
+    public void turnOn() {
+        System.out.println("Radio is on");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Radio is off");
+    }
+}
