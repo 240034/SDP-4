213a2d2745ee67bbe71d4707451834691009fd68
240034 <240034@astanait.edu.kz>
2025-10-04 20:21:39 +0500

    Create TV.java

---
+++ TV.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public class TV implements Device {
+    @Override
+    public void turnOn() {
+        System.out.println("TV is on");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV is off");
+    }
+}
