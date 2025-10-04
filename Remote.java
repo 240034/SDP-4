90cf2cb5eb5e266c2f1a86510f0a9025c9592966
240034 <240034@astanait.edu.kz>
2025-10-04 20:21:13 +0500

    Create Remote.java

---
+++ Remote.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public abstract class Remote {
+    protected Device device;
+
+    protected Remote(Device device) {
+        this.device = device;
+    }
+
+    public abstract void togglePower();
+}
